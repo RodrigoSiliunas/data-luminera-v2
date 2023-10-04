706f05c259b66ce1289ee4ea72f1c8644badb489
Rodrigo Siliunas <rodrigo.siliunas98@outlook.com>
2023-10-04 05:26:26 -0300

    fix(monsters): Monstros V5

--- monster/reptiles/cobra.lua
+++ monster/reptiles/cobra.lua
@@ -1,10 +1,10 @@
-local mType = Game.createMonsterType("Snake")
+local mType = Game.createMonsterType("Cobra")
 local monster = {}
 
-monster.description = "a snake"
-monster.experience = 10
+monster.description = "a cobra"
+monster.experience = 30
 monster.outfit = {
-	lookType = 28,
+	lookType = 81,
 	lookHead = 0,
 	lookBody = 0,
 	lookLegs = 0,
@@ -13,29 +13,30 @@ monster.outfit = {
 	lookMount = 0,
 }
 
-monster.raceId = 28
+monster.raceId = 81
 monster.Bestiary = {
 	class = "Reptile",
 	race = BESTY_RACE_REPTILE,
-	toKill = 250,
-	FirstUnlock = 10,
-	SecondUnlock = 100,
-	CharmsPoints = 5,
-	Stars = 1,
+	toKill = 500,
+	FirstUnlock = 25,
+	SecondUnlock = 250,
+	CharmsPoints = 15,
+	Stars = 2,
 	Occurrence = 0,
-	Locations = "In almost all grassy areas of Tibia. Also found in Rookgaard.",
+	Locations = "Ankrahmun Library Tomb, Tarpit Tomb, Mountain Tomb, Peninsula Tomb, Darama, Tiquanda, \z
+		Drefia, Forbidden Lands, Arena Quarter, Lion's Rock.",
 }
 
-monster.health = 15
-monster.maxHealth = 15
+monster.health = 65
+monster.maxHealth = 65
 monster.race = "blood"
-monster.corpse = 3998
+monster.corpse = 4188
 monster.speed = 60
-monster.manaCost = 205
+monster.manaCost = 275
 
 monster.changeTarget = {
 	interval = 4000,
-	chance = 0,
+	chance = 10,
 }
 
 monster.strategiesTarget = {
@@ -47,10 +48,10 @@ monster.flags = {
 	attackable = true,
 	hostile = true,
 	convinceable = false,
-	pushable = true,
+	pushable = false,
 	rewardBoss = false,
 	illusionable = true,
-	canPushItems = false,
+	canPushItems = true,
 	canPushCreatures = false,
 	staticAttackChance = 90,
 	targetDistance = 1,
@@ -70,30 +71,34 @@ monster.light = {
 monster.voices = {
 	interval = 5000,
 	chance = 10,
-	{ text = "Zzzzzzt", yell = false },
+	{ text = "Fsssss", yell = false },
+	{ text = "Zzzzzz", yell = false },
 }
 
-monster.loot = {}
+monster.loot = {
+	{ name = "cobra tongue", chance = 5000 },
+}
 
 monster.attacks = {
-	{ name = "melee", interval = 2000, chance = 100, minDamage = 0, maxDamage = -8, condition = { type = CONDITION_POISON, totalDamage = 15, interval = 4000 } },
+	{ name = "melee", interval = 2000, chance = 100, minDamage = 0, maxDamage = 0, condition = { type = CONDITION_POISON, totalDamage = 100, interval = 4000 } }, -- poison
+	{ name = "condition", type = CONDITION_POISON, interval = 2000, chance = 15, minDamage = -20, maxDamage = -40, range = 7, shootEffect = CONST_ANI_POISON, target = false },
 }
 
 monster.defenses = {
 	defense = 5,
-	armor = 0,
-	mitigation = 0.03,
+	armor = 1,
+	mitigation = 0.15,
 }
 
 monster.elements = {
 	{ type = COMBAT_PHYSICALDAMAGE, percent = 0 },
-	{ type = COMBAT_ENERGYDAMAGE, percent = 20 },
-	{ type = COMBAT_EARTHDAMAGE, percent = 40 },
+	{ type = COMBAT_ENERGYDAMAGE, percent = 0 },
+	{ type = COMBAT_EARTHDAMAGE, percent = 100 },
 	{ type = COMBAT_FIREDAMAGE, percent = -10 },
 	{ type = COMBAT_LIFEDRAIN, percent = 0 },
 	{ type = COMBAT_MANADRAIN, percent = 0 },
 	{ type = COMBAT_DROWNDAMAGE, percent = 0 },
-	{ type = COMBAT_ICEDAMAGE, percent = -10 },
+	{ type = COMBAT_ICEDAMAGE, percent = 0 },
 	{ type = COMBAT_HOLYDAMAGE, percent = 0 },
 	{ type = COMBAT_DEATHDAMAGE, percent = 0 },
 }
--- monster/reptiles/thornback_tortoise.lua
+++ monster/reptiles/thornback_tortoise.lua
@@ -1,10 +1,10 @@
-local mType = Game.createMonsterType("Tortoise")
+local mType = Game.createMonsterType("Thornback Tortoise")
 local monster = {}
 
-monster.description = "a tortoise"
-monster.experience = 90
+monster.description = "a thornback tortoise"
+monster.experience = 150
 monster.outfit = {
-	lookType = 197,
+	lookType = 198,
 	lookHead = 0,
 	lookBody = 0,
 	lookLegs = 0,
@@ -13,7 +13,7 @@ monster.outfit = {
 	lookMount = 0,
 }
 
-monster.raceId = 258
+monster.raceId = 259
 monster.Bestiary = {
 	class = "Reptile",
 	race = BESTY_RACE_REPTILE,
@@ -23,15 +23,15 @@ monster.Bestiary = {
 	CharmsPoints = 15,
 	Stars = 2,
 	Occurrence = 0,
-	Locations = "Laguna Islands, Fenrock, Port Hope and also can be found behind a wall below the center of Liberty Bay. These cannot be reached.",
+	Locations = "Laguna Islands, Meriana Gargoyle Cave and one on Nargor.",
 }
 
-monster.health = 185
-monster.maxHealth = 185
+monster.health = 300
+monster.maxHealth = 300
 monster.race = "blood"
-monster.corpse = 6072
-monster.speed = 65
-monster.manaCost = 445
+monster.corpse = 6073
+monster.speed = 75
+monster.manaCost = 490
 
 monster.changeTarget = {
 	interval = 5000,
@@ -50,8 +50,8 @@ monster.flags = {
 	pushable = false,
 	rewardBoss = false,
 	illusionable = true,
-	canPushItems = false,
-	canPushCreatures = false,
+	canPushItems = true,
+	canPushCreatures = true,
 	staticAttackChance = 90,
 	targetDistance = 1,
 	runHealth = 0,
@@ -73,27 +73,31 @@ monster.voices = {
 }
 
 monster.loot = {
-	{ name = "gold coin", chance = 59000, maxCount = 30 },
-	{ name = "battle hammer", chance = 730 },
-	{ name = "plate shield", chance = 2850 },
-	{ id = 3578, chance = 4600 }, -- fish
-	{ name = "tortoise egg", chance = 770, maxCount = 2 },
-	{ id = 5899, chance = 1300 }, -- turtle shell
-	{ name = "tortoise shield", chance = 200 },
+	{ name = "white pearl", chance = 1600 },
+	{ name = "black pearl", chance = 800 },
+	{ name = "gold coin", chance = 89500, maxCount = 48 },
+	{ name = "war hammer", chance = 260 },
+	{ id = 3578, chance = 10800, maxCount = 2 }, -- fish
+	{ name = "white mushroom", chance = 1200 },
+	{ name = "brown mushroom", chance = 700 },
+	{ name = "tortoise egg", chance = 790, maxCount = 3 },
+	{ id = 5899, chance = 800 }, -- turtle shell
+	{ name = "health potion", chance = 1600 },
+	{ name = "thorn", chance = 15980 },
 }
 
 monster.attacks = {
-	{ name = "melee", interval = 2000, chance = 100, minDamage = 0, maxDamage = -50 },
+	{ name = "melee", interval = 2000, chance = 100, minDamage = 0, maxDamage = -110, condition = { type = CONDITION_POISON, totalDamage = 40, interval = 4000 } },
 }
 
 monster.defenses = {
-	defense = 30,
-	armor = 22,
-	mitigation = 0.51,
+	defense = 40,
+	armor = 24,
+	mitigation = 0.70,
 }
 
 monster.elements = {
-	{ type = COMBAT_PHYSICALDAMAGE, percent = 20 },
+	{ type = COMBAT_PHYSICALDAMAGE, percent = 30 },
 	{ type = COMBAT_ENERGYDAMAGE, percent = 0 },
 	{ type = COMBAT_EARTHDAMAGE, percent = 20 },
 	{ type = COMBAT_FIREDAMAGE, percent = -10 },
