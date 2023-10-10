18917e3fcbfd679b2767338820a3baddf7170894
Rodrigo Siliunas <rodrigo.siliunas98@outlook.com>
2023-10-10 16:12:22 -0300

    feat(action): Falcon V0.0.1

--- scripts/actions/other/request_falcon.lua
+++ scripts/actions/other/request_falcon.lua
@@ -1,7 +1,11 @@
+local http = require('socket.http')
 local falcon = Action()
 
 function falcon.onUse(player, item, fromPosition, target, toPosition, isHotkey)
+    local result, status = http.request('http://www.google.com')
+
     player:getPosition():sendMagicEffect(CONST_ME_SOUND_YELLOW)
+    player:sendTextMessage("You have successfully on send a request to google.com, status:" .. status)
     return true
 end
 
