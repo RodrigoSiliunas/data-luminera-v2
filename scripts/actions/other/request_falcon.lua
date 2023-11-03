04e572e8467527bfdc74d091e0728f6581e54003
Rodrigo Siliunas <rodrigo.siliunas98@outlook.com>
2023-11-02 22:47:03 -0300

    Request FALCON atualizado

--- scripts/actions/other/request_falcon.lua
+++ scripts/actions/other/request_falcon.lua
@@ -9,7 +9,11 @@ function falcon.onUse(player, item, fromPosition, target, toPosition, isHotkey)
     -- Envia a solicitação GET usando o método sendGetRequest
     local response_body = {}
     local response_code = Webhook.sendGetRequest(apiUrl, customHeaders, response_body)
-    print("Requisição Efetuada. Status: " .. response_code .. "\nBody: " .. response_body)
+    
+    -- Converte a tabela response_body em uma string
+    local response_string = table.concat(response_body)
+    
+    print("Requisição Efetuada. Status: " .. response_code .. "\nBody: " .. response_string)
 
     player:getPosition():sendMagicEffect(CONST_ME_SOUND_YELLOW)
     return true
