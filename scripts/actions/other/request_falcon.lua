caae99bed4a45d2ad5494efb2e44b0378ccfd495
Rodrigo Siliunas <rodrigo.siliunas98@outlook.com>
2023-11-02 22:55:50 -0300

    Adição do Falcon Request

--- scripts/actions/other/request_falcon.lua
+++ scripts/actions/other/request_falcon.lua
@@ -4,18 +4,17 @@ function falcon.onUse(player, item, fromPosition, target, toPosition, isHotkey)
     local customHeaders = {
         ["Content-Type"] = "application/json"
     }
-    local apiUrl = "https://pokeapi.co/api/v2/pokemon/pikachu/"
+    local apiUrl = "https://jsonplaceholder.typicode.com/posts/1"
 
     -- Envia a solicitação GET usando o método sendGetRequest
     local response_body = {}
-    local response_code, response = Webhook.sendGetRequest(apiUrl, customHeaders, response_body)
+    local response_code = Webhook.sendGetRequest(apiUrl, customHeaders, response_body)
     
     -- Converte a tabela response_body em uma string
     local response_string = table.concat(response_body)
     
-    -- print("Requisição Efetuada. Status: " .. response_code .. "\nBody: " .. response_string)
-
-    print(response)
+    -- Imprime a resposta
+    print("Requisição Efetuada. Status: " .. response_code .. "\nBody: " .. response_string)
 
     player:getPosition():sendMagicEffect(CONST_ME_SOUND_YELLOW)
     return true
